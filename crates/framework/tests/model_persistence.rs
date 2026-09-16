c35bbeab63ae38fbc1c2d572679a9c2b7b924e82
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-16 16:12:00 +0400

    Verify Claw persistence inside request transaction

--- crates/framework/tests/model_persistence.rs
+++ crates/framework/tests/model_persistence.rs
@@ -3,8 +3,8 @@
 use framework::{
     claw::{field, Model, PersistableModel, Row, Value},
     database::{
-        Capabilities, Connection, Database, DatabaseError, Driver, ErrorKind, Execution, Statement,
-        Transaction, TransactionOptions,
+        Capabilities, Connection, Database, Driver, Execution, Statement, Transaction,
+        TransactionOptions,
     },
     App, FromJson, Headers, Json, Method, Request, Response, Result, ValidateInput, Validated,
     ValidationErrors,
@@ -75,6 +75,7 @@ impl ValidateInput for UserInput {
 
 struct FakeConnection {
     executed: Arc<Mutex<Vec<Statement>>>,
+    commits: Arc<AtomicUsize>,
 }
 
 impl Connection for FakeConnection {
@@ -108,13 +109,41 @@ impl Connection for FakeConnection {
         &mut self,
         _options: TransactionOptions,
     ) -> framework::database::Result<Box<dyn Transaction + '_>> {
-        Err(DatabaseError::new(
-            ErrorKind::Transaction,
-            "transactions are not used by this test",
-        ))
+        Ok(Box::new(FakeTransaction {
+            executed: Arc::clone(&self.executed),
+            commits: Arc::clone(&self.commits),
+        }))
     }
 
     fn ping(&mut self) -> framework::database::Result<()> {
+        Ok(())
+    }
+}
+
+struct FakeTransaction {
+    executed: Arc<Mutex<Vec<Statement>>>,
+    commits: Arc<AtomicUsize>,
+}
+
+impl Transaction for FakeTransaction {
+    fn execute(&mut self, statement: &Statement) -> framework::database::Result<Execution> {
+        self.executed.lock().unwrap().push(statement.clone());
+        Ok(Execution {
+            affected_rows: 1,
+            last_insert_id: None,
+        })
+    }
+
+    fn query(&mut self, _statement: &Statement) -> framework::database::Result<Vec<Row>> {
+        Ok(Vec::new())
+    }
+
+    fn commit(self: Box<Self>) -> framework::database::Result<()> {
+        self.commits.fetch_add(1, Ordering::SeqCst);
+        Ok(())
+    }
+
+    fn rollback(self: Box<Self>) -> framework::database::Result<()> {
         Ok(())
     }
 }
@@ -126,36 +155,95 @@ fn update(mut user: User, input: Validated<UserInput>, request: Request) -> Resu
     Ok(Response::empty().status(204))
 }
 
+fn update_transaction(
+    mut user: User,
+    input: Validated<UserInput>,
+    request: Request,
+) -> Result<Response> {
+    user.name = input.name.clone();
+    request.transaction(TransactionOptions::default(), |connection| {
+        user.save(connection)?;
+        Ok(())
+    })?;
+    Ok(Response::empty().status(204))
+}
+
+fn body_request(path: &str) -> Request {
+    let mut headers = Headers::new();
+    headers.insert("content-type", "application/json").unwrap();
+    Request::new(
+        Method::new("PUT").unwrap(),
+        path,
+        headers,
+        br#"{"name":"  Grace  "}"#.to_vec(),
+    )
+    .unwrap()
+}
+
 #[test]
 fn bound_model_validated_input_and_save_share_one_connection() {
     let acquisitions = Arc::new(AtomicUsize::new(0));
     let executed = Arc::new(Mutex::new(Vec::new()));
+    let commits = Arc::new(AtomicUsize::new(0));
     let factory_acquisitions = Arc::clone(&acquisitions);
     let factory_executed = Arc::clone(&executed);
+    let factory_commits = Arc::clone(&commits);
     let mut app = App::new();
     app.database(Database::new(move || {
         factory_acquisitions.fetch_add(1, Ordering::SeqCst);
         Ok(FakeConnection {
             executed: Arc::clone(&factory_executed),
+            commits: Arc::clone(&factory_commits),
         })
     }))
     .unwrap();
     app.route().put("/users/{user}", update).unwrap();
 
-    let mut headers = Headers::new();
-    headers.insert("content-type", "application/json").unwrap();
-    let request = Request::new(
-        Method::new("PUT").unwrap(),
-        "/users/7",
-        headers,
-        br#"{"name":"  Grace  "}"#.to_vec(),
-    )
-    .unwrap();
-
-    let response = app.handle(request).unwrap();
+    let response = app.handle(body_request("/users/7")).unwrap();
 
     assert_eq!(response.status_code(), 204);
     assert_eq!(acquisitions.load(Ordering::SeqCst), 1);
+    assert_eq!(commits.load(Ordering::SeqCst), 0);
+    let executed = executed.lock().unwrap();
+    assert_eq!(executed.len(), 1);
+    assert_eq!(
+        executed[0].sql(),
+        "UPDATE \"users\" SET \"name\" = ? WHERE \"id\" = ?"
+    );
+    assert_eq!(
+        executed[0].bindings(),
+        &[Value::Text("Grace".into()), Value::U64(7)]
+    );
+}
+
+#[test]
+fn claw_save_works_inside_request_transaction_on_same_connection() {
+    let acquisitions = Arc::new(AtomicUsize::new(0));
+    let executed = Arc::new(Mutex::new(Vec::new()));
+    let commits = Arc::new(AtomicUsize::new(0));
+    let factory_acquisitions = Arc::clone(&acquisitions);
+    let factory_executed = Arc::clone(&executed);
+    let factory_commits = Arc::clone(&commits);
+    let mut app = App::new();
+    app.database(Database::new(move || {
+        factory_acquisitions.fetch_add(1, Ordering::SeqCst);
+        Ok(FakeConnection {
+            executed: Arc::clone(&factory_executed),
+            commits: Arc::clone(&factory_commits),
+        })
+    }))
+    .unwrap();
+    app.route()
+        .put("/users/{user}/transaction", update_transaction)
+        .unwrap();
+
+    let response = app
+        .handle(body_request("/users/7/transaction"))
+        .unwrap();
+
+    assert_eq!(response.status_code(), 204);
+    assert_eq!(acquisitions.load(Ordering::SeqCst), 1);
+    assert_eq!(commits.load(Ordering::SeqCst), 1);
     let executed = executed.lock().unwrap();
     assert_eq!(executed.len(), 1);
     assert_eq!(
