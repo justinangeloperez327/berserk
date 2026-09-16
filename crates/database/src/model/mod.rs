a9bc372f7cdb085f1ea942ddca88d9084722997e
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-16 10:47:23 +0400

    Remove duplicate database ORM module

--- crates/database/src/model/mod.rs
+++
@@ -1,9 +0,0 @@
-mod decode;
-mod query;
-mod record;
-mod relationship;
-
-pub use decode::{field, FromValue};
-pub use query::ModelQuery;
-pub use record::Model;
-pub use relationship::{BelongsTo, HasMany, HasOne, RelatedSet};
