33f70fe959a47cc8e476bb249c30b8e7a3bece8a
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-16 13:26:42 +0400

    Export resource routing traits

--- crates/framework/src/lib.rs
+++ crates/framework/src/lib.rs
@@ -40,7 +40,9 @@ pub use framework_storage as storage;
 pub use http::{Headers, HttpError, IntoResponse, Method, Request, Response, StatusCode};
 
 pub mod routing;
-pub use routing::{Route, RouteError};
+pub use routing::{
+    ApiResourceController, NamedRoute, ResourceController, Route, RouteError,
+};
 
 pub mod server;
 pub use server::Server;
