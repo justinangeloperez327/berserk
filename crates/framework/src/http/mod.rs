9f861db0aabab3eee3366a4283f59273afcc1db4
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-16 15:35:56 +0400

    Export request connection guard

--- crates/framework/src/http/mod.rs
+++ crates/framework/src/http/mod.rs
@@ -10,5 +10,7 @@ pub use error::HttpError;
 pub use headers::Headers;
 pub use method::Method;
 pub use request::Request;
+#[cfg(feature = "database")]
+pub use request::RequestConnection;
 pub use response::{IntoResponse, Response};
 pub use status::StatusCode;
