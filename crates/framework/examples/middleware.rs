08fcce37596cffd085bbd6ca6968f7cc82a631c7
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-20 10:16:12 +0400

    fix(1.0): migrate remaining route calls in middleware.rs

--- crates/framework/examples/middleware.rs
+++ crates/framework/examples/middleware.rs
@@ -7,7 +7,7 @@ fn main() -> Result<()> {
         next.run(request)?.header("x-service", "example")
     });
     app.group("/api", |routes| {
-        routes.get("/name", |req: Request| {
+        routes.route().get("/name", |req: Request| {
             Response::text(req.state::<String>().unwrap().clone())
         })
     })?;
